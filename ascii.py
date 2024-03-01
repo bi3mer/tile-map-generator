1b0b974373c8c03427442a9698ed461890dafad4
bi3mer <colan@comcast.net>
2024-03-01 10:35:48 -0500

    ascii version cleaned up

--- ascii.py
+++ ascii.py
@@ -6,19 +6,7 @@ from bit_operations import get_bit_mask, get_bit_mask_ignore_corners
 IGNORE_TILE = "."
 
 
-def read_file(file_path):
-    map = []
-    solids = []
-
-    with open(file_path) as f:
-        for line in f.readlines():
-            map.append(list(line.strip()))
-            solids.append([c != IGNORE_TILE for c in line.strip()])
-
-    return map, solids
-
-
-if __name__ == "__main__":
+def parser():
     parser = argparse.ArgumentParser("ASCII Tile Map Decorator")
     parser.add_argument(
         "--example",
@@ -31,6 +19,7 @@ if __name__ == "__main__":
         help="Convert a level given an example level",
         type=str,
         required=False,
+        default=None,
     )
     parser.add_argument(
         "--output-map",
@@ -43,21 +32,26 @@ if __name__ == "__main__":
         action="store_true",
         required=False,
     )
-    args = parser.parse_args()
 
-    # TODO: handle tile size if image in arg parse
+    return parser.parse_args()
 
-    example_level = args.example
 
+def read_file(file_path):
+    map = []
+    solids = []
+
+    with open(file_path) as f:
+        for line in f.readlines():
+            map.append(list(line.strip()))
+            solids.append([c != IGNORE_TILE for c in line.strip()])
+
+    return map, solids
+
+
+def read_example_level(example_level, bitmask_finder):
     if not os.path.exists(example_level):
         print(f"Could not read the example level: {example_level}")
-
-    if args.ignore_corners:
-        print(1)
-        bitmask_finder = get_bit_mask_ignore_corners
-    else:
-        bitmask_finder = get_bit_mask
-        print(2)
+        exit(-1)
 
     map, solids = read_file(example_level)
     bitmaskToTile = {}
@@ -78,11 +72,15 @@ if __name__ == "__main__":
             else:
                 bitmaskToTile[bitmask] = char
 
-    convert_level = args.convert
-    if not os.path.exists(convert_level):
-        print(f"Could not read level to convert: {convert_level}")
+    return bitmaskToTile
 
-    convert_map, convert_solids = read_file(convert_level)
+
+def convert_level(level, bitmask_finder, bitmaskToTile):
+    if not os.path.exists(level):
+        print(f"Could not read level to convert: {level}")
+        exit(-1)
+
+    convert_map, convert_solids = read_file(level)
 
     for y in range(len(convert_map)):
         for x in range(len(convert_map[0])):
@@ -94,3 +92,21 @@ if __name__ == "__main__":
                 convert_map[y][x] = bitmaskToTile[bitmask]
 
     print("\n".join("".join(line) for line in convert_map))
+
+
+def main():
+    args = parser()
+
+    if args.ignore_corners:
+        bitmask_finder = get_bit_mask_ignore_corners
+    else:
+        bitmask_finder = get_bit_mask
+
+    bitmaskToTile = read_example_level(args.example, bitmask_finder)
+
+    if args.convert:
+        convert_level(args.convert, bitmask_finder, bitmaskToTile)
+
+
+if __name__ == "__main__":
+    main()
